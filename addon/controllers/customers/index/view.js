ab662a8507e6a43e594bf3efbef5f4464bd2ddce
Turtuvshin <toroo.byamba@gmail.com>
2024-03-12 19:22:12 +0800

    Add customer panel and related components and routes

---
+++ addon/controllers/customers/index/view.js
@@ -0,0 +1,44 @@
+import Controller from '@ember/controller';
+import BaseController from '@fleetbase/fleetops-engine/controllers/base-controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class CustomersIndexViewController extends BaseController {
+    /**
+     * The currently active view tab ('details' by default).
+     *
+     * @type {String}
+     * @tracked
+     */
+    @tracked view = 'details';
+
+    /**
+     * An array of query parameters to be serialized in the URL.
+     *
+     * @type {String[]}
+     * @tracked
+     */
+    @tracked queryParams = ['view'];
+
+    /**
+     * Transitions back to the "management.contacts.index" route.
+     *
+     * @method
+     * @action
+     * @returns {Transition} The transition object representing the route change.
+     */
+    @action transitionBack() {
+        return this.transitionToRoute('management.contacts.index');
+    }
+
+    /**
+     * Updates the active view tab.
+     *
+     * @method
+     * @param {String} tab - The name of the tab to activate.
+     * @action
+     */
+    @action onTabChanged(tab) {
+        this.view = tab;
+    }
+}
